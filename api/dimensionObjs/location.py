a7628a54e12ba8160a11d59644498664a8b04f60
ch942 <ch942@drexel.edu>
2022-03-02 02:05:06 -0500

    Added dimension objects temporal and location

---
+++ api/dimensionObjs/location.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+class Location:
+    """
+    Location Object
+    """
+    def __init__(self, location_uid, value):
+        self.location_uid = location_uid
+        self.location_val = value
---
+++ api/dimensionObjs/temporal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+class Temporal:
+    """
+    Temporal Object
+    """
+    def __init__(self, temporal_uid, value):
+        self.temporal_uid = temporal_uid
+        self.temporal_val = value
