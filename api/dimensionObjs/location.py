e4a9944ee20efa1baff2292094eb0241f716da28
ch942 <ch942@drexel.edu>
2022-03-02 12:44:56 -0500

    removed temp and loc objects, added to scripts

--- api/dimensionObjs/location.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-class Location:
-    """
-    Location Object
-    """
-    def __init__(self, location_uid, value):
-        self.location_uid = location_uid
-        self.location_val = value
--- api/dimensionObjs/temporal.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-class Temporal:
-    """
-    Temporal Object
-    """
-    def __init__(self, temporal_uid, value):
-        self.temporal_uid = temporal_uid
-        self.temporal_val = value
