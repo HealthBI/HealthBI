2ad2fd27e9c60d10ad891b67476bc23dab1b75ed
chrislunch <cjl334@drexel.edu>
2022-04-18 11:52:30 -0400

    Adds submit button functionality for dictionary mappings

--- Website/HealthBI/urls.py
+++ Website/HealthBI/urls.py
@@ -3,6 +3,7 @@ from .views import upload_page
 from .views import analysis
 from .views import index
 from .views import upload_csv
+from .views import upload_mappings
 
 urlpatterns = [
     path('', index, name='index'),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('upload/', upload_page, name='uploadCsvWithMappings'),
     path('', analysis, name='analysis'),
     path('upload/csv/$', upload_csv, name='upload_csv'),
+    path('upload/mappings/$', upload_mappings, name='upload_mappings'),
 
 ]
