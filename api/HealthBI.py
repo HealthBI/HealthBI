2cd1526c9ddbf40bcc08cb95648b5e3a6f3aa860
ch942 <ch942@drexel.edu>
2022-05-17 12:20:23 -0400

    API upload

--- api/HealthBI.py
+++ api/HealthBI.py
@@ -6,8 +6,7 @@
 #     Corinna Hoang
 #     Trang Hoang 
 #     Chris Lynch 
-#     Charles Porter 
-#     Angie Margai-Ren
+#     Devang Rungta
 # '''
 
 import os
@@ -27,18 +26,18 @@ class HealthBI:
     - correlate_dataset
     - select
     """
-    def __init__(self, csv_file, json_file):
-        self.csv_file = csv_file
-        self.json_file = json_file
+    def __init__(self):
+        self.csv_file = None
+        self.json_file = None
         self.inject = None
         # Connect to database.
-        self.conn, self.cursor = self.connect_to_database()
+        # self.conn, self.cursor = self.connect_to_database()
         # Run shaping.
         self.shape = None
-        self.shape_csv()
-        self.inject_shaped_csv()
-        print("Data successfully processed.\n")
-        self.conn.close()
+        # self.shape_csv()
+        # self.inject_shaped_csv()
+        # print("Data successfully processed.\n")
+        # self.conn.close()
     
     def connect_to_database(self):
         """
@@ -105,14 +104,16 @@ class HealthBI:
         self.conn.commit()
 
 if __name__=="__main__":
-    if len(sys.argv) == 3:
-        csv_exists = os.path.exists(sys.argv[1])
-        json_exists = os.path.exists(sys.argv[2])
-        if csv_exists == True and json_exists == True:
-            run = HealthBI(sys.argv[1], sys.argv[2])
-        elif csv_exists == False:
-            print("CSV file does not exist.")
-        elif json_exists == False:
-            print("JSON file does not exist.")
-    else:
-        print("Usage: python HealthBI.py csvFile jsonFile")
+    healthbi = HealthBI()
+    if len(sys.argv) == 4:
+        if sys.argv[1] == "upload":
+            csv_exists = os.path.exists(sys.argv[2])
+            json_exists = os.path.exists(sys.argv[3])
+            if csv_exists == True and json_exists == True:
+                run = healthbi.upload_dataset(sys.argv[2], sys.argv[3])
+            elif csv_exists == False:
+                print("CSV file does not exist.")
+            elif json_exists == False:
+                print("JSON file does not exist.")
+        else:
+            print("Usage: python ./api/HealthBI.py upload csvFile jsonFile")
